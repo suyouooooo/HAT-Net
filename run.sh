b1641c0d495afde8cc5cd0f3276208a4a3f4532a
weiaicunzai <372995411@qq.com>
2021-05-11 19:59:07 +0800

    fix: add stage, depth args to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #export CUDA_VISIBLE_DEVICES=1 && python train_HGP_GCN.py --batch_size 10
 export CUDA_VISIBLE_DEVICES=1 
-#/home/by/miniconda3/bin/conda activate 
-/home/syh/anaconda3/envs/CGC-Net2/bin/python train_GIN_Hierarchical.py --load_data_list --norm_adj --batch-size=10 --step_size=10 --jk
+python train_GIN_Hierarchical.py --load_data_list --norm_adj --batch-size=10 --step_size=10 --jk  --depth 6  --stage  2
