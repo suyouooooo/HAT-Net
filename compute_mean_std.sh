a1de1f8e4357e2b087a3c6b3e531a2b8dda7e76e
weiaicunzai <372995411@qq.com>
2021-07-28 16:21:59 +0800

    feat: save work

--- compute_mean_std.sh
+++ compute_mean_std.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #export CUDA_VISIBLE_DEVICES=1 && python train_HGP_GCN.py --batch_size 10
-export CUDA_VISIBLE_DEVICES=1
-export CUDA_HOME=$CUDA_HOME:/home/syh/cuda-11.0
-export PATH=$PATH:/home/syh/cuda-11.0/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/syh/cuda-11.0/lib64
+#export CUDA_VISIBLE_DEVICES=1
+#export CUDA_HOME=$CUDA_HOME:/home/syh/cuda-11.0
+#export PATH=$PATH:/home/syh/cuda-11.0/bin
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/syh/cuda-11.0/lib64
 
-source /home/syh/anaconda3/bin/activate
+source /home/suyihan/anaconda3/bin/activate
 #which conda
-which python
 #conda activate CGC-Net2
-conda activate CGC-Net2-tensorboard
+conda activate CGC-Net_by
+
+which python
+#conda activate CGC-Net2-tensorboard
 #cd /data/by/tmp/HGIN
 
-python -u /data/by/tmp/HGIN/compute_mean.py
+python -u compute_mean.py
